8ffde5c54165f28c696b9377ac267d1082a0a8eb
Jim Grimes <jimgrimes86@gmail.com>
2023-09-20 21:35:52 -0400

    completed lab tasks; unable to get tests to work

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Add your code here
+function submitData (userName, userEmail) {
+    const userInfo ={
+        name: userName,
+        email: userEmail,
+    };
+
+    const postObject = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+        },
+        body: JSON.stringify(userInfo),
+    }
+
+    const body = document.querySelector('body');
+    
+    return fetch('http://localhost:3000/users', postObject)
+    .then((response) => response.json())
+    .then((object) => {
+        const userId = document.createElement('div');
+        userId.innerText = object.id;
+        body.append(userId)
+    })
+    .catch(error => {
+        const errorMessage = document.createElement('div');
+        errorMessage.innerText - error.message;
+        body.append(errorMessage)
+    })
+}
+// submitData("John Doe", "email@email.com")
